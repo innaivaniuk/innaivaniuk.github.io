53664e3640331c831c34d48a8439370132f5ed28
Inna Ivaniuk <innaivaniuk20@gmail.com>
2019-03-20 19:49:11 +0100

    poprawka

--- zadanie-domowe/Zadanie15721/js/main.js
+++ zadanie-domowe/Zadanie15721/js/main.js
@@ -1,18 +1,23 @@
-$(function(){
- 
+$(function () {
+
     $('.button').click(function () {
 
-    $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) { 
-       
-    $('#dane-programisty').after(
-        '<br> Imię:'+ data.imie + 
-        '<br> Nazwisko:' + data.nazwisko + 
-        '<br> Zawod:' + data.zawod + 
-        '<br> Firma:' + data.firma)
-       })
+        $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
 
-    }); 
-         
-}); 
+        
 
-    
+            $('.button').after($('<div/>', {
+                id: 'dane-programisty'
+            }));
+
+            $('#dane-programisty').append(
+                '<br> Imię:' + data.imie +
+                '<br> Nazwisko:' + data.nazwisko +
+                '<br> Zawod:' + data.zawod +
+                '<br> Firma:' + data.firma)
+        })
+
+    });
+
+});
+
